0e246e074724548421706e0f8dd486f7783a7519
takamisris <takamisris@gmail.com>
2018-04-08 17:26:05 +0900

    install semantic ui

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    <div class="ui huge menu">
+      <a class="item">
+        Home
+      </a>
+      <a class="item">
+        About Us
+      </a>
+      <a class="item">
+        Products
+      </a>
+      <a class="item">
+        Contact Us
+      </a>
+    </div>
+
+    <h2 class="ui center aligned icon header">
+      <i class="circular pencil alternate icon"></i>
+      <h2>Hand &nbsp; Writing &nbsp; Generator</h2>
+    </h2>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+    <button class="positive big ui button">Upload Image</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style>
+  .main{
+    margin: 50px
+  }
+</style>
