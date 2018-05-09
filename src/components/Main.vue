735f92fb5c6d9bfbe58e6913b2405a15ec7ec1bb
takamisris <takamisris@gmail.com>
2018-05-09 23:04:46 +0900

    add axios

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,46 @@
 <template>
   <div class="main">
-    <button class="positive big ui button">Upload Image</button>
+    <form>
+      <input type="file" id="image" name="file" ref="file" v-on:change="handleFileUpload()" />
+      <button class="positive ui button" v-on:click="submitFile()">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
+const axios = require('axios')
 export default {
-    name: 'Main'
+  name: 'Main',
+  datq() {
+    return {
+      file: ''
+    }
+  },
+  methods: {
+    handleFileUpload() {
+      // set a local variable to the value of the file uploaded
+      this.file = this.$refs.file.files[0]
+    },
+    submitFile() {
+      let formData = new FormData()
+      if (this.file !== '') {
+        formData.append('file', this.file)
+      }
+      
+      axios.post('http://54.201.208.182:3000/image', formData,{
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+      })
+      .then(function(res) {
+        console.log(res)
+      })
+      .catch(function() {
+        console.log('failed')
+      })
+
+    }
+  }
 }
 </script>
 
